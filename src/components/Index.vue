63a714b00ad38aae6dcceb830d49384c1e9bf36c
MaybeHadBabel <heyuboom93@qq.com>
2019-08-28 17:17:37 +0800

    首页完成

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,8 +1,51 @@
 <template>
   <el-container class="index">
-  <el-header>Header</el-header>
+  <el-header>
+    <div class="logo">
+      <img src="../assets/logo.ec55f206.png" alt="">
+    </div>
+    <div class="title">
+      <h1>电商后台管理系统</h1>
+    </div>
+    <div class="logout">
+      <span>欢迎光临~ </span>
+      <a href="javascript:;" @click='logout'>退出</a>
+    </div>
+  </el-header>
   <el-container>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="200px">
+      <el-menu
+      default-active='1-1'
+      unique-opened
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template>
+        <el-menu-item index="1-1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">用户列表</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>权限管理</span>
+        </template>
+        <el-menu-item index="2-1">
+          <i class="el-icon-menu"></i>
+          <span slot="title">角色列表</span>
+        </el-menu-item>
+        <el-menu-item index="2-2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">权限列表</span>
+        </el-menu-item>
+      </el-submenu>
+    </el-menu>
+    </el-aside>
     <el-main>Main</el-main>
   </el-container>
 </el-container>
@@ -10,18 +53,63 @@
 
 <script>
 export default {
-
+  methods: {
+    logout () {
+      this.$confirm('亲，确定要退出系统吗？？', '温馨提示', {
+        type: 'warning'
+      }).then(() => {
+        this.$message('成功退出')
+        localStorage.removeItem('token')
+        this.$router.push('/login')
+      }).catch((e) => {
+        console.log(e)
+      })
+    }
+  }
 }
 </script>
 
-<style lang='scss'>
+<style lang='scss' scoped>
 .index{
   height: 100%;
   .el-header {
     background-color: #d8d8d8;
+    display: flex;
+    .logo {
+      width: 180px;
+      margin: 10px;
+      img {
+        height: 40px;
+      }
+    }
+    .logout {
+      width: 180px;
+      text-align: right;
+      height: 60px;
+      line-height: 60px;
+      span {
+        font-weight: 700;
+      }
+      a {
+        font-weight: 700;
+        color: orange;
+      }
+    }
+    .title {
+      flex: 1;
+      h1 {
+        height: 60px;
+        line-height: 60px;
+        color: #545c64;
+        text-align: center;
+      }
+    }
   }
   .el-aside {
     background-color: #545c64;
+    .el-menu {
+      border-right: none;
+    }
   }
   .el-main {
     background-color: #ecf0f1;
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -68,7 +68,7 @@ export default {
 }
 </script>
 
-<style lang='scss'>
+<style lang='scss' scoped>
   .login {
     width: 100%;
     height: 100%;
